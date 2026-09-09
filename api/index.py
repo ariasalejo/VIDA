4744866205f40145d9a5df42df2089c57f93d470
ariasalejo <ariasalejolondo@gmail.com>
2026-09-09 18:48:02 -0500

    feat: SSL certificates, storage optimization, Piper TTS, CORS - Complete setup for Fedora

--- api/index.py
+++ api/index.py
@@ -1,5 +1,11 @@
 import os
 import sys
+import certifi
+
+# Configurar certificados SSL para Fedora/Linux
+os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+os.environ['SSL_CERT_FILE'] = certifi.where()
+os.environ['CURL_CA_BUNDLE'] = certifi.where()
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
